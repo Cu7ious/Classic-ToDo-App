9b90a5c2bebf8062d01c82cfa8e3f24ea9c36cfc
CU7IOUS <garkusha.sergiy@gmail.com>
2016-12-22 09:14:30 +0200

    Initial commit

---
+++ node.js
@@ -0,0 +1,9 @@
+// var http = require('http');
+//
+// var server = new http.Server();
+//
+// server.listen(3000, '127.0.0.1');
+//
+// server.on('request', (request, response) => {
+//   response.end('Hello World');
+// });
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export default {
+  CONSTANTS: {
+    ALL: 'all',
+    REMAINED: 'remained',
+    COMPLETED: 'completed'
+  },
+  items: [
+    {text: 'Add persistency layer to the App', done: false, editing: false},
+    {text: 'Develop To-Do App with React', done: false, editing: false},
+    {text: 'Assemble environment for App', done: false, editing: false}
+  ]
+}
